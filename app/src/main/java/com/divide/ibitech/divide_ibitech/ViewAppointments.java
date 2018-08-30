e40eaa71f018d95434641b1f3f42419557093f79
lufefe <jumbaayabonga@gmail.com>
2018-08-30 06:58:56 +0200

    Iteration 3

---
+++ app/src/main/java/com/divide/ibitech/divide_ibitech/ViewAppointments.java
@@ -0,0 +1,13 @@
+package com.divide.ibitech.divide_ibitech;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewAppointments extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_appointments);
+    }
+}
